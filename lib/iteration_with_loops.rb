8caa6eefd5dfe07b98cd4a8fb7275ff56d20d0d7
Anna Nicole Reed <reedanna@mail.com>
2020-06-22 19:53:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -17,7 +17,7 @@ def find_even_values(src)
     end
     outer_count += 1
   end
-  print return_array
+  return_array
 end
 
-find_even_values([[1, 2], [3, 4]])
+#find_even_values([[1, 2], [3, 4]])
